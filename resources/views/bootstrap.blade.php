165663d37d013e66114a99b57750d942fefb3c6e
unknown <gonchiperedo77@gmail.com>
2020-08-14 18:44:50 -0400

    hice modificaciones en el template de bootstrap

--- resources/views/bootstrap.blade.php
+++ resources/views/bootstrap.blade.php
@@ -8,10 +8,10 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Hola usfa!</title>
   </head>
   <body>
-    <h1>Hello, world!</h1>
+    <h1>Hola usfa!</h1>
     <button class="btn btn-primary">Ingresar al sistema</button>
 
     <!-- Optional JavaScript -->
